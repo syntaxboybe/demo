ece833829d7009f42a9ef17b4f9a7460469b2201
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-06-28 02:29:25 +0800

    feat: day1 upskilling

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Fun from './Fun.svelte';
+  import Funny from './Funny.svelte';
+</script>
+
+<h1>Hello Boybe Svelte</h1>
+<Fun />
+<Funny />
---
+++ src/Fun.svelte
@@ -0,0 +1,1 @@
+<h2>Forgotten Once:</h2>
