8a4079ac5c8c327c3fd1b61ddba2db983f307cfe
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-07-04 15:57:09 +0800

    feat: DOM Events

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,54 @@
 <script>
-  import Fun from './Fun.svelte';
-  import Funny from './Funny.svelte';
+    let left = 100;
+    let top = 100;
+    function clickMe(e) {
+      console.log(e);
+      alert('Clicked');
+    }
+    function onInput(e) {
+      console.log(e.target.value, 'input');
+    }
+
+    function onChange(e) {
+      console.log(e, 'change');
+    }
+
+    function onKeyup(e) {
+       switch (e.key) {
+        case 'ArrowUp':
+          top -= 50;
+          break;
+        case 'ArrowDown':
+          top += 50;
+          break;
+        case 'ArrowLeft':
+          left -= 50;
+          break;
+        case 'ArrowRight':
+          left += 50;
+          break;
+
+       }
+  }
+      function handleSubmit(e) {
+      console.log('handlesubmit');
+    }
+
 </script>
 
-<h1>Hello Boybe Svelte</h1>
-<Fun />
-<Funny />
+<button on:click|once={clickMe}>Click Me</button>
+<input type="range" on:input={onInput} on:change={onChange}/>
+<svelte:window on:keydown={onKeyup} />
+<form on:submit|stopPropagation|preventDefault={handleSubmit}>
+   <button type="submit">Submit Form</button>
+</form>
+<div style="left: {left}px; top: {top}px;" />
+
+<style>
+ div {
+   width: 100px;
+   height: 100px;
+   position: absolute;
+   background-color: aqua;
+ }
+</style>
